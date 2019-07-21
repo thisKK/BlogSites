d07b9085f85574b58f8ebfc94d453b4dbd7675ca
thisKK <42506615+thisKK@users.noreply.github.com>
2019-07-21 23:17:22 +0700

    success
    fdgdfgdfgd

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 export const FETCH_POST = 'fetch_post';
 export const CREATE_POST = 'create_posts';
+export const DELETE_POST = 'deleate_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=PAPERCLIP5123';
@@ -26,9 +27,19 @@ export function createPost(values,callback){
 }
 
 export function fetchPost(id){
-    const request = axios.post(`${ROOT_URL}/posts/${id}${API_KEY}`);
+    const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
     return {
         type: FETCH_POST,
         payload: request
+    };
+}
+
+export function deletePost(id,callback){
+    const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+        .then(()=>callback());
+        
+    return {
+        type: DELETE_POST,
+        payload: id
     };
 }
--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions';
+import { fetchPost,deletePost } from '../actions';
 import {Link} from 'react-router-dom';
 
 
@@ -11,7 +11,9 @@ class PostShow extends Component {
     }
     onDeleteClick(){
         const {id} = this.props.match.params;
-        this.props.deletePost(id);
+        this.props.deletePost(id,()=> {
+            this.props.history.push('/');
+        });
     }
 
     render() {
@@ -40,4 +42,4 @@ function mapStateToProps({posts},ownProps){
     return {post: posts[ownProps.match.params.id]};
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostShow);
+export default connect(mapStateToProps, { fetchPost ,deletePost})(PostShow);
